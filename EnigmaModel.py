97b624f8eb1dea00aecec111b99d520944b762b9
Oliver <oliverlnordqvist@gmail.com>
2025-12-05 00:18:30 +0100

    commited again for just changes

--- EnigmaModel.py
+++ EnigmaModel.py
@@ -1,28 +1,39 @@
 # File: EnigmaModel.py
 
-""" This is the starter file for the Enigma project. """
 from EnigmaView import EnigmaView
 from EnigmaRotor import EnigmaRotor
-from EnigmaConstants import ROTOR_PERMUTATIONS
+from EnigmaRotor import apply_permutation_forward, apply_permutation_backward
+from EnigmaConstants import ROTOR_PERMUTATIONS, REFLECTOR_PERMUTATION
+
+
+# Supporter functions to make conversion easier
+def letter_to_index(letter):
+    return ord(letter) - ord('A')
+
+def index_to_letter(index):
+    return chr(ord('A') + index)
+
 
 class EnigmaModel:
-
+    # Initializes the Enigma model
     def __init__(self):
-        """Creates a new EnigmaModel with no views."""
-        self._views = [ ]
+        self._views = []
 
+        # Checks the state of the buttons on the Enigma keyboard
         self._key_states = {chr(i): False for i in range(ord('A'), ord('Z') + 1)}
+
+        # Checks the state of the lamps on the Enigma lampboard
         self._lamp_states = {chr(i): False for i in range(ord('A'), ord('Z') + 1)}
 
         # Create three rotors using the permutations defined in EnigmaConstants
         self._rotors = [EnigmaRotor(p) for p in ROTOR_PERMUTATIONS]
 
+    # Adds a view to the list of views
     def add_view(self, view):
-        """Adds a view to this model."""
         self._views.append(view)
 
+#   Sends an update request to all the views.
     def update(self):
-        """Sends an update request to all the views."""
         for view in self._views:
             view.update()
 
@@ -32,32 +43,75 @@ class EnigmaModel:
     def is_lamp_on(self, letter):
         return self._lamp_states.get(letter, False)
 
+
     def key_pressed(self, letter):
-        self._key_states[letter] = True
-        self._lamp_states[letter] = True  #light up the same lettter for which key has been decided to press
+        fast = self._rotors[2]     # right rotor
+        medium = self._rotors[1]  # middle rotor
+        slow = self._rotors[0]   # left rotor
+
+        # Advances the fast/right rotor. If it wraps around, advance the medium rotor, if that also the continue to slow rotor
+        carry = fast.advance()
+        if carry:
+            carry2 = medium.advance()
+            if carry2:
+                slow.advance()
+
+        # converts the pressed to an index 0-25
+        index = letter_to_index(letter)
+
+        # Forward (right -> left)
+        for rotor in reversed(self._rotors):
+            index = apply_permutation_forward(
+                index,
+                rotor.get_forward_permutation(),
+                rotor.get_offset()
+            )
+
+        # Find reflected letter and convert it from A to Z and back to a 0-25 index
+        index = ord(REFLECTOR_PERMUTATION[index]) - ord('A')
+
+        # Backward (left to right)
+        for rotor in self._rotors:
+            index = apply_permutation_backward(
+                index,
+                rotor.get_backward_permutation(),
+                rotor.get_offset()
+            )
+
+        # Convert index -> letter and light that lamp
+        encrypted_letter = index_to_letter(index)
+        self._lamp_states[encrypted_letter] = True
+
         self.update()
+
 
     def key_released(self, letter):
         self._key_states[letter] = False
-        self._lamp_states[letter] = False  #turn off the lamp when key is released
+
+        # Turn off ALL lamps when any key is released
+        for L in self._lamp_states:
+            self._lamp_states[L] = False
+
         self.update()
 
+    # Returns the letter showing on rotor <index>
     def get_rotor_letter(self, index):
         rotor = self._rotors[index]
         offset = rotor.get_offset()
-        return chr(ord('A') + offset)
-
+        return index_to_letter(offset)
+    
+    # Returns the offset (0-25) of rotor <index>
     def rotor_clicked(self, index):
         self._rotors[index].advance()
         self.update()
 
+
 def enigma():
-    """Runs the Enigma simulator."""
     model = EnigmaModel()
     view = EnigmaView(model)
     model.add_view(view)
 
-# Startup code
 
+# Startup code
 if __name__ == "__main__":
     enigma()
--- EnigmaRotor.py
+++ EnigmaRotor.py
@@ -1,20 +1,50 @@
 # File: EnigmaRotor.py
-# """This module defines the EnigmaRotor class for the Enigma simulator."""
 
+#  Invert a rotor permutation so that we can go backwards through it
+def invert_key(permutation):
+    inverse = [''] * 26
+    for i, ch in enumerate(permutation):
+        idx = ord(ch) - ord('A')   #Get the encrypted letter
+        inverse[idx] = chr(ord('A') + i) #Map back to the original letter
+    return ''.join(inverse)
+
+#Class EnigmaRotor that we call upon in the EnigmaModel.py
 class EnigmaRotor:
     def __init__(self, permutation):
-        """Initializes a rotor with its permutation and sets offset to 0."""
-        self._permutation = permutation
+        self._forward_permutation = permutation
+        self._backward_permutation = invert_key(permutation)
         self._offset = 0
 
+    #Returns the current offset (0–25). used when applying the rotor permutation
     def get_offset(self):
-        """Returns the current offset (0–25)."""
         return self._offset
+    # returns the forward permutation string
+    def get_forward_permutation(self):
+        return self._forward_permutation
+    # returns the backward permutation string
+    def get_backward_permutation(self):
+        return self._backward_permutation
 
-    def get_permutation(self):
-        """Returns the permutation string for this rotor."""
-        return self._permutation
 
     def advance(self):
-        """Advances the rotor by one position, cycling after 25."""
         self._offset = (self._offset + 1) % 26
+        return self._offset == 0  
+    # Return True if wrapped around from Z then back to A, rerutns a boolean TRUE if thats the case
+
+
+
+#   apply_permutation
+def apply_permutation_forward(index, permutation, offset):
+    shifted_index = (index + offset) % 26
+    permuted_char = permutation[shifted_index]
+    permuted_index = ord(permuted_char) - ord('A')
+    final_index = (permuted_index - offset) % 26
+    return final_index
+
+def apply_permutation_backward(index, permutation, offset):
+    shifted_index = (index + offset) % 26
+    permuted_char = permutation[shifted_index]
+    permuted_index = ord(permuted_char) - ord('A')
+    final_index = (permuted_index - offset) % 26
+    return final_index
+    
